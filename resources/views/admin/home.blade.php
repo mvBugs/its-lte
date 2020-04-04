d21ef9beed29099f3409aebd44dbbe6d0fac31f5
Микола Войтович <m.voitovych@ideil.com>
2020-04-04 16:02:00 +0300

    fix home admin

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -13,34 +13,36 @@
     <section class="content">
         <div class="row">
             @foreach($locations as $location)
+                @if($location->slug == 'usharal')
                 <div class="col-md-3">
-                <div class="box">
-                    <div class="box-body">
+                    <div class="box">
+                        <div class="box-body">
 
-                        <!--tabs-->
+                            <!--tabs-->
 
-                        <div class="nav-tabs-justified">
-                            <div class="tab-content">
-                                <h3>{{ $location->name }}</h3>
-                                <div class="tab-pane active" id="tab_1">
-                                    <br>
-                                    {!! Form::model($location, [
-                                        'method' => 'PATCH',
-                                        'route' => ['admin.location.update', $location],
-                                    ]) !!}
-                                        <div class="form-group {{ $errors->has('commission') ? 'has-error' : ''}}">
-                                            {!! Form::label('commission', 'Коммисия', ['class' => 'control-label']) !!}
-                                            {!! Form::number('commission', null, ['class' => 'form-control', 'required', 'min'=>0,'max'=>100]) !!}
-                                            {!! $errors->first('commission', '<p class="help-block">:message</p>') !!}
-                                        </div>
-                                        <button class="btn btn-success">Сохранить</button>
-                                    {!! Form::close() !!}
+                            <div class="nav-tabs-justified">
+                                <div class="tab-content">
+                                    <h3>{{ $location->name }}</h3>
+                                    <div class="tab-pane active" id="tab_1">
+                                        <br>
+                                        {!! Form::model($location, [
+                                            'method' => 'PATCH',
+                                            'route' => ['admin.location.update', $location],
+                                        ]) !!}
+                                            <div class="form-group {{ $errors->has('commission') ? 'has-error' : ''}}">
+                                                {!! Form::label('commission', 'Коммисия', ['class' => 'control-label']) !!}
+                                                {!! Form::number('commission', null, ['class' => 'form-control', 'required', 'min'=>0,'max'=>100]) !!}
+                                                {!! $errors->first('commission', '<p class="help-block">:message</p>') !!}
+                                            </div>
+                                            <button class="btn btn-success">Сохранить</button>
+                                        {!! Form::close() !!}
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
+                @endif
             @endforeach
         </div>
     </section>
